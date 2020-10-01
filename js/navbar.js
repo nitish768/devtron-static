e7a085b2685c906ec9e067fd0a12476d4c2b7335
Rashmi Rai <rashmi.rai@devtron.ai>
2020-10-01 14:22:03 +0530

    section why

--- js/navbar.js
+++ js/navbar.js
@@ -3,11 +3,14 @@ $(function () {
   var $sectionOne = $(".section-hero");
   var $navbar = $(".main-nav");
   $(document).scroll(function () {
+    if ($(this).scrollTop() == 0) {
+      $navbar.removeClass("main-nav-dark main-nav-light");
+    }
     if ($(this).scrollTop() > $navbar.height()) {
       $navbar.addClass("main-nav-light");
       $navbar.removeClass("main-nav-dark");
     }
-    else if ($(this).scrollTop() > $sectionOne.height()) {
+    if ($(this).scrollTop() > $sectionOne.height()) {
       $navbar.removeClass("main-nav-light");
       $navbar.addClass("main-nav-dark");
     }
