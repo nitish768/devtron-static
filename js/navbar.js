dc9e3d41a182297c9a97350405e1c9c6f2d36088
Rashmi Rai <rashmi.rai@devtron.ai>
2020-10-03 08:22:54 +0530

    navbar for chrome

--- js/navbar.js
+++ js/navbar.js
@@ -1,20 +1,17 @@
 var image1 = $('#image-1')
 var image2 = $('#image-2')
 
-$(function () {
-  var $sectionOne = $(".section-hero");
-  var $navbar = $(".main-nav");
+var $sectionOne = $(".section-hero");
+var $navbar = $(".main-nav");
+var $sectionTwo = $(".section-why");
+var $sectionThree = $(".section-landscape");
 
-  var $sectionTwo = $(".section-why");
-  var $sectionThree = $(".section-landscape");
+$(function () {
 
   $(document).scroll(function () {
 
     if ($(this).scrollTop() == 0) {
       $navbar.removeClass("main-nav-dark main-nav-light");
-
-      $(image1).removeClass('fixed-pos');
-      $(image2).removeClass('fixed-pos');
     }
 
     if ($(this).scrollTop() > $navbar.height()) {
@@ -22,34 +19,30 @@ $(function () {
       $navbar.removeClass("main-nav-dark");
     }
 
-    if ($(this).scrollTop() > $sectionOne.height()) {
+    if ($(this).scrollTop() > ($sectionOne.height() + 2 * $navbar.height())) {
       $navbar.removeClass("main-nav-light");
       $navbar.addClass("main-nav-dark");
-
-      $(image1).addClass('fixed-pos')
-      $(image2).addClass('fixed-pos')
-      $(image1).removeClass('hidden')
+      $(image1).removeClass('fixed-pos fixed-pos__current-landscape');
+      $(image2).removeClass('fixed-pos fixed-pos__vision-landscape');
     }
 
-    if ($(this).scrollTop() > $sectionTwo.height()) {
-      $navbar.removeClass("main-nav-light");
-      $navbar.addClass("main-nav-dark");
-
-      $(image1).addClass('fixed-pos')
-      $(image2).addClass('fixed-pos')
-      $(image1).removeClass('hidden')
+    if ($(this).scrollTop() > $sectionOne.height() + 431) {
+      console.log($(this).scrollTop());
+      $(image1).addClass('fixed-pos fixed-pos__current-landscape')
+      $(image2).addClass('fixed-pos fixed-pos__vision-landscape')
+      // $(image1).removeClass('hidden')
     }
 
-    if ($(this).scrollTop() > 1510) {
-      console.log(window.scrollY);
-
-      $navbar.removeClass("main-nav-light");
-      $navbar.addClass("main-nav-dark");
-
+    if ($(this).scrollTop() > ($sectionOne.height() + $sectionTwo.height() + (5 * $navbar.height()))) {
+      $navbar.addClass("main-nav-light");
+      $navbar.removeClass("main-nav-dark");
       $(image1).removeClass('fixed-pos')
       $(image2).removeClass('fixed-pos')
-      $(image1).removeClass('hidden')
     }
 
+    if ($(this).scrollTop() > ($sectionOne.height() + $sectionTwo.height() + $sectionThree.height() + (7 * $navbar.height()))) {
+      $navbar.removeClass("main-nav-light");
+      $navbar.addClass("main-nav-dark");
+    }
   });
 });
