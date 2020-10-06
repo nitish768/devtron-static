72fde2a57700a58922d2883b14557d6efbcdb058
Rashmi Rai <rashmi.rai@devtron.ai>
2020-10-06 20:02:49 +0530

    deleted files

--- wp-content/plugins/pixxy-plugins/includes/composer/map.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-include_once( EF_ROOT . '/includes/composer/params.php' );
-
-if ( ! function_exists( 'is_plugin_active' ) ) {
-	include_once( ABSPATH . 'wp-admin/includes/plugin.php' ); // Require plugin.php to use is_plugin_active() below
-}
