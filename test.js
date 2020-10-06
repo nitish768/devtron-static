e1ecfa9daa0242ed89a745182c655225167986c6
hardik12 <hardik@devtron.ai>
2020-10-06 21:07:39 +0530

    timer added

--- test.js
+++ test.js
@@ -9,3 +9,31 @@ function handleTwitterIconClick (itemCountInClass) {
     // const linkedinURL = `https://www.linkedin.com/shareArticle?mini=true&&url=https://stackoverflow.com/questions/10713542/how-to-make-custom-linkedin-share-button/10737122&title=How%20to%20make%20custom%20linkedin%20share%20button&summary=${currentItemContent}`
     window.open(twitterURL, '_blank');
 }
+
+function setRemainigTime() {
+    const countDownDate = new Date("Oct 15, 2020 00:00:01").getTime();
+
+    let x = setInterval(() => {
+
+    const now = new Date().getTime();
+  
+    const distance = countDownDate - now;
+  
+    // Time calculations for days, hours, minutes and seconds
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  
+    // Display the result in the element with id="demo"
+    document.getElementById("ävailableTime").innerText = 'Available In ' + days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+  
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("ävailableTime").innerText = "Check out our product!!";
+    }
+  }, 1000);
+}
+
+setRemainigTime()
