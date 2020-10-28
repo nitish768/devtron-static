3ed2b14b8ba3bbbbedba82957baf7a9eae7c2d4b
Rashmi Rai <rashmi.rai@devtron.ai>
2020-10-28 14:00:39 +0530

    scrollout commented

--- js/product.js
+++ js/product.js
@@ -239,11 +239,11 @@ $(document).ready(function () {
   //   }
   // }
 
-  ScrollOut({
-    targets: ".section-why__grid-item",
-    threshold: 0.3,
-    once: true,
-  });
+  // ScrollOut({
+  //   targets: ".section-why__grid-item",
+  //   threshold: 0.3,
+  //   once: true,
+  // });
 
   getGithubStars();
 
