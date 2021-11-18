fac090f57452b865b527e036f6147fc5d7311dda
shivani170 <shivani@devtron.ai>
2021-11-18 07:57:39 +0530

    removed unnecessary js file

--- public/js/support.js
+++
@@ -1,7 +0,0 @@
-function viewOnAdoptK8sGAEvent(label) {
-    gtag('event', 'View on #AdoptK8sWithDevtron Nav Button Clicked', {
-      'event_category': 'View on #AdoptK8sWithDevtron',
-      'event_label': label
-    });
-    console.log('hi')
-  }
